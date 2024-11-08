32f56e8c7c4347daef5b30800300b9cfef212fae
Juan Barrera <jcbp1999@gmail.com>
2024-11-08 19:43:54 +1100

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from argparse import ArgumentParser
+
+parser = ArgumentParser(
+    prog='TaskManager',
+    description='Task Manager for adding, removing and editing tasks on the current project'
+)
+
+
+
+
+
